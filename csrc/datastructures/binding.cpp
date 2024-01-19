330b6aa264d503e45c84c5ea0519e99f784e1adc
Birkenpapier <kevin.peivareh@apollogic.com>
2024-01-19 14:52:36 +0100

    added bindings and header for c++

---
+++ csrc/datastructures/binding.cpp
@@ -0,0 +1,2 @@
+//  build with: g++ -o binding binding.cpp -L. -lpdcs -Idatastructures
+#include "libpdcs.hpp"
---
+++ csrc/datastructures/libpdcs.hpp
@@ -0,0 +1,17 @@
+// libpdcs.hpp
+
+#ifndef LIBPDCS_HPP
+#define LIBPDCS_HPP
+
+// headers for each data structure
+#include "datastructures/bloomfilter/bloomfilter.hpp"
+#include "datastructures/fenwick_tree/fenwick_tree.hpp"
+#include "datastructures/hashtable/HashTable.hpp"
+#include "datastructures/k_d_tree_2d/k_d_tree_2d.hpp"
+#include "datastructures/queue/queue.hpp"
+#include "datastructures/ringbuffer/ringbuffer.hpp"
+// #include "datastructures/segment_tree/segment_tree.hpp"
+#include "datastructures/stack/stack.hpp"
+#include "datastructures/trie/trie.hpp"
+
+#endif // LIBPDCS_HPP
