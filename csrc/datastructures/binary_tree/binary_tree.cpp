0c5bcab73abcdb695b26132eba8517092be1b2c0
Birkenpapier <kevin.peivareh@apollogic.com>
2024-01-20 11:34:48 +0100

    finished btree implementation

--- csrc/datastructures/binary_tree/binary_tree.cpp
+++ csrc/datastructures/binary_tree/binary_tree.cpp
@@ -1,10 +1,38 @@
+#include "binary_tree.hpp"
 /**
  * Definition for a binary tree node.
- * struct BTNode {
- *     int data;
- *     BTNode *LNode;
- *     BTNode *RNode;
- *     BTNode(int x) : val(x), left(NULL), right(NULL) {}
+ * class BTNode {
+ *     public:
+ *         BTNode*  lNode;
+ *         BTNode*  rNode;
+ *         int data;
+ * 
+ *         // insert new data
+ *         BTNode(int val) : data(val), lNode(nullptr), rNode(nullptr) {}
  * };
  */
 
+int main() {
+    BTree btree;
+
+    btree.insert(5);
+    btree.insert(3);
+    btree.insert(6);
+    btree.insert(8);
+    btree.insert(9);
+    btree.insert(2);
+    btree.insert(1);
+
+    std::cout << "in order traversal" << std::endl;
+    btree.inOrder();
+
+    int valToSearch = 1;
+    std::cout << "looking for " << valToSearch << ": "
+        << (btree.search(valToSearch) ? "found" : "not found") << std::endl;
+
+    valToSearch = 10;
+    std::cout << "looking for " << valToSearch << ": "
+        << (btree.search(valToSearch) ? "found" : "not found") << std::endl;
+
+    return 0;
+}
--- csrc/datastructures/binary_tree/binary_tree.hpp
+++ csrc/datastructures/binary_tree/binary_tree.hpp
@@ -1,8 +1,80 @@
-#include <unordered_map>
+#include <iostream>
 
 class BTNode {
     public:
-        std::unordered_map<char, BTNode*> LNode;
-        std::unordered_map<char, BTNode*> RNode;
+        BTNode*  lNode;
+        BTNode*  rNode;
         int data;
+
+        // insert new data
+        BTNode(int val) : data(val), lNode(nullptr), rNode(nullptr) {}
+};
+
+class BTree {
+    private:
+        BTNode* root;
+
+        BTNode* insert(BTNode* n, int val) {
+            if(n == nullptr) {
+                return new BTNode(val);
+            }
+
+            if(val < n->data) {
+                n->lNode = insert(n->lNode, val);
+            } else {
+                n->rNode = insert(n->rNode, val);   
+            }
+
+            return n;
+        }
+
+        void inOrder(BTNode* n) {
+            if(n != nullptr) {
+                inOrder(n->lNode);
+                std::cout << n->data << " ";
+                inOrder(n->rNode);
+            }
+        }
+
+        bool search(BTNode* n, int val) {
+            if(n == nullptr) {
+                return false;
+            }
+            if(n->data == val) {
+                return true;
+            }
+            if(val < n->data) {
+                return search(n->lNode, val);
+            } else {
+                return search(n->rNode, val);
+            }
+        }
+
+        void free(BTNode* n) {
+            if(n) {
+                free(n->lNode);
+                free(n->rNode);
+                delete n;
+            }
+        }
+
+    public:
+        BTree() : root(nullptr) {}
+
+        ~BTree() {
+            free(root);
+        }
+
+        void insert(int val) {
+            root = insert(root, val);
+        }
+
+        void inOrder() {
+            inOrder(root);
+            std::cout << std::endl;
+        }
+
+        bool search(int val) {
+            return search(root, val);
+        }
 };
