bf61e4edfefb2b5d7f0a0225a54eb9ad4d608da1
Birkenpapier <kevin@peivareh.com>
2024-01-23 19:27:46 +0100

    header file for a start algorithm

--- csrc/algorithms/a_star/a_star.hpp
+++ csrc/algorithms/a_star/a_star.hpp
@@ -96,23 +96,3 @@ std::vector<Node> aStar(const std::vector<std::vector<int>>& grid, Node start, N
 
     return {};  // No path found
 }
-
-int main() {
-    std::vector<std::vector<int>> grid = {
-        {0, 0, 0, 1, 0},
-        {1, 1, 0, 1, 0},
-        {0, 0, 0, 0, 0},
-        {0, 1, 1, 1, 1},
-        {0, 0, 0, 0, 0}
-    };
-
-    Node start(0, 0);
-    Node end(4, 4);
-
-    auto path = aStar(grid, start, end);
-    for (auto& node : path) {
-        std::cout << "(" << node.x << "," << node.y << ") ";
-    }
-
-    return 0;
-}
