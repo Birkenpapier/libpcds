3a5e81ec80b6308f62da376eb40dcfbff774fb67
Birkenpapier <kevin@peivareh.com>
2024-01-23 19:46:57 +0100

    better g_scan

--- csrc/algorithms/graham_scan/g_scan.cpp
+++ csrc/algorithms/graham_scan/g_scan.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stack>
+#include <cmath>
 
-#include "stack.hpp"
-#include "g_scan.hpp"
-
-using namespace std;
+struct Point {
+    int x, y;
+};
 
 Point p0;
 
-Point nextToTop(Stack<Point> &S) {
-    Point p = S.top();
-    S.pop();
-    Point res = S.top();
-    S.push(p);
-    return res;
+int distSq(const Point &p1, const Point &p2) {
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+int orientation(const Point &p, const Point &q, const Point &r) {
+    int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+    if (val == 0) return 0;
+    return (val > 0) ? 1 : 2;
 }
 
 void swap(Point &p1, Point &p2) {
@@ -23,34 +26,25 @@ void swap(Point &p1, Point &p2) {
     p2 = temp;
 }
 
-int distSq(Point p1, Point p2) {
-    return (p1.x - p2.x) * (p1.x - p2.x) +
-           (p1.y - p2.y) * (p1.y - p2.y);
-}
-int orientation(Point p, Point q, Point r) {
-    int val = (q.y - p.y) * (r.x - q.x) -
-              (q.x - p.x) * (r.y - q.y);
-
-    if (val == 0) return 0;
-    return (val > 0) ? 1 : 2;
+Point nextToTop(std::stack<Point> &S) {
+    Point p = S.top();
+    S.pop();
+    Point res = S.top();
+    S.push(p);
+    return res;
 }
 
-int compare(const void *vp1, const void *vp2) {
-   Point *p1 = (Point *)vp1;
-   Point *p2 = (Point *)vp2;
-
-   int o = orientation(p0, *p1, *p2);
-   if (o == 0)
-     return (distSq(p0, *p2) >= distSq(p0, *p1)) ? -1 : 1;
-
-   return (o == 2) ? -1 : 1;
+bool compare(const Point &p1, const Point &p2) {
+    int o = orientation(p0, p1, p2);
+    if (o == 0)
+        return distSq(p0, p2) >= distSq(p0, p1);
+    return (o == 2);
 }
 
-void convexHull(Point points[], int n) {
+void convexHull(std::vector<Point> points) {
     int ymin = points[0].y, min = 0;
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < points.size(); i++) {
         int y = points[i].y;
-
         if ((y < ymin) || (ymin == y && points[i].x < points[min].x))
             ymin = points[i].y, min = i;
     }
@@ -58,14 +52,13 @@ void convexHull(Point points[], int n) {
     swap(points[0], points[min]);
 
     p0 = points[0];
-    qsort(&points[1], n-1, sizeof(Point), compare);
+    std::sort(points.begin() + 1, points.end(), compare);
 
     int m = 1;
-    for (int i=1; i<n; i++) {
-        while (i < n-1 && orientation(p0, points[i],
-                                       points[i+1]) == 0)
+    for (int i = 1; i < points.size(); i++) {
+        while (i < points.size() - 1 && orientation(p0, points[i],
+                points[i + 1]) == 0)
             i++;
-
 
         points[m] = points[i];
         m++;
@@ -73,29 +66,28 @@ void convexHull(Point points[], int n) {
 
     if (m < 3) return;
 
-    Stack<Point> S;
+    std::stack<Point> S;
     S.push(points[0]);
     S.push(points[1]);
     S.push(points[2]);
 
     for (int i = 3; i < m; i++) {
-        while (orientation(nextToTop(S), S.top(), points[i]) != 2)
+        while (S.size() > 1 && orientation(nextToTop(S), S.top(), points[i]) != 2)
             S.pop();
         S.push(points[i]);
     }
 
     while (!S.empty()) {
         Point p = S.top();
-        cout << "(" << p.x << ", " << p.y <<")" << endl;
+        std::cout << "(" << p.x << ", " << p.y << ")" << std::endl;
         S.pop();
     }
 }
 
 int main() {
-    Point points[] = {{0, 3}, {1, 1}, {2, 2}, {4, 4},
-                      {0, 0}, {1, 2}, {3, 1}, {3, 3}};
-    int n = sizeof(points)/sizeof(points[0]);
-    convexHull(points, n);
+    std::vector<Point> points = {{0, 3}, {1, 1}, {2, 2}, {4, 4},
+                                 {0, 0}, {1, 2}, {3, 1}, {3, 3}};
+    convexHull(points);
 
     return 0;
 }
